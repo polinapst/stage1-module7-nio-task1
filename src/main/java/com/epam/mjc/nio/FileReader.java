ee6566d102d088e1ded8fbf7c990b8c66df505a0
polina <polinatuzankina@gmail.com>
2023-12-10 15:05:03 +0600

    NIO task - finished

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,71 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        String filePath = file.getPath();
+        String s;
+
+        try (RandomAccessFile aFile = new RandomAccessFile(filePath, "r")) {
+            FileChannel inChannel = aFile.getChannel();
+
+            long fileSize = inChannel.size();
+
+            ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
+            inChannel.read(buffer);
+            buffer.flip();
+
+            s = new String(buffer.array());
+            return new Profile(extractName(s), extractAge(s), extractEmail(s), extractPhone(s));
+        } catch (FileNotFoundException e) {
+            System.out.println("No file was found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("An Input/Output exception occured when reading the file");
+            e.printStackTrace();
+        }
+
         return new Profile();
+    }
+
+    private String extractInfo(String input, String infoType) {
+        String[] sArray = input.split("\n");
+
+        for (String st: sArray) {
+            if (st.toLowerCase().startsWith(infoType.toLowerCase())) {
+                return st.split(" ")[1];
+            }
+        }
+        return "Not found";
+    }
+
+    private String extractName(String input) {
+        return extractInfo(input, "Name").trim();
+    }
+
+    private Integer extractAge(String input) {
+        try {
+            return Integer.parseInt(extractInfo(input, "Age").trim());
+        }
+        catch (IllegalArgumentException e) {
+            return -1;
+        }
+    }
+
+    private String extractEmail(String input) {
+        return extractInfo(input, "Email").trim();
+    }
+
+    private Long extractPhone(String input) {
+        try {
+            return Long.parseLong(extractInfo(input, "Phone").trim());
+        }
+        catch (IllegalArgumentException e) {
+            return -1L;
+        }
     }
 }
